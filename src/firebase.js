2e4729d909ebfb4c6b4c8832e488dfb08c3fd90f
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2023-07-25 19:34:37 +0400

    fix issue 2

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_APP_MESSAGING_SENDER,
-  appId: process.env.VITE_APP_APP_ID,
+  apiKey: import.meta.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.VITE_APP_MESSAGING_SENDER,
+  appId: import.meta.VITE_APP_APP_ID,
 };
 
 // Initialize Firebase
