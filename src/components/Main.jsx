7ac1d71cddc11d28cd9c3f4cd065d4650c0c4f14
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2023-07-28 12:35:38 +0400

    dsadas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,6 +10,7 @@ const Main = () => {
       setMovies(response.data.results);
     });
   }, []);
+
   const movie = movies[Math.floor(Math.random() * movies.length)];
 
   const trunaceString = (str, num) => {
@@ -20,35 +21,37 @@ const Main = () => {
     }
   };
 
-  return (
-    <div className="w-full h-[550px]">
-      <div className="w-full h-full">
-        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
-        <img
-          className="w-full h-full object-cover block"
-          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-          alt={movie?.title}
-        />
-        <div className="absolute w-full top-[20%] p-4">
-          <h1 className="text-3xl text-gray-200 md:text-5xl font-bold mb-3">
-            {movie?.title}
-          </h1>
-          <div className="mb-3">
-            <button className="px-5 py-2 bg-gray-400 text-black mr-3">
-              Play
-            </button>
-            <button className="px-5 py-2 border border-white text-white">
-              Watch Later
-            </button>
+  {
+    return movies ? (
+      <div className="w-full h-[550px]">
+        <div className="w-full h-full">
+          <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+          <img
+            className="w-full h-full object-cover block"
+            src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+            alt={movie?.title}
+          />
+          <div className="absolute w-full top-[20%] p-4">
+            <h1 className="text-3xl text-gray-200 md:text-5xl font-bold mb-3">
+              {movie?.title}
+            </h1>
+            <div className="mb-3">
+              <button className="px-5 py-2 bg-gray-400 text-black mr-3">
+                Play
+              </button>
+              <button className="px-5 py-2 border border-white text-white">
+                Watch Later
+              </button>
+            </div>
+            <h1 className="text-gray-400">{movie?.release_date}</h1>
+            <h1 className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+              {trunaceString(movie?.overview, 150)}
+            </h1>
           </div>
-          <h1 className="text-gray-400">{movie?.release_date}</h1>
-          <h1 className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
-            {trunaceString(movie?.overview, 150)}
-          </h1>
         </div>
       </div>
-    </div>
-  );
+    ) : null;
+  }
 };
 
 export default Main;
